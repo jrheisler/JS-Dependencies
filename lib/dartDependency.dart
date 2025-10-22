050baacc8a5dbf2c3474738efc48ff5839be7cbb
Jeff Heisler <jrheisler@yahoo.com>
2025-10-22 14:05:32 -0700

    Fix dart:io File usage

--- lib/dartDependency.dart
+++ lib/dartDependency.dart
@@ -19,7 +19,7 @@ import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/file_system/file_system.dart';
+import 'package:analyzer/file_system/file_system.dart' show PathNotFoundException;
 import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:analyzer/source/line_info.dart';
 import 'package:analyzer/source/source.dart';
